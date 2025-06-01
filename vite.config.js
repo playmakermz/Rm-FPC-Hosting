d2630726faa2ce0f982ac928d709de46ccfe6ce4
Playmakermz🍉 <60807663+playmakermz@users.noreply.github.com>
2025-06-01 09:19:19 +0000

    re deploy

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
 
 export default defineConfig({
-  base: "/Rm_FPC_Hosting/",
   plugins: [vue()],
-  resolve: {
-    alias: {
-      vue: resolve(__dirname, 'node_modules/vue/dist/vue.esm-bundler.js'),
-      '@': resolve(__dirname, 'src') // Jika menggunakan alias '@'
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ['vue'] // Pastikan Vue tidak di-bundle ulang
-    }
-  }
+  base: "/Rm-FPC-Hosting",
 });
