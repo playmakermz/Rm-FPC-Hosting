47fbc294d711d1c775a08cee50c1976f57086721
Playmakermz🍉 <60807663+playmakermz@users.noreply.github.com>
2025-06-01 07:59:07 +0000

    new

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 //import vue from '@vitejs/plugin-vue'
-import vue from './node_modules/@vitejs/plugin-vue/dist/index.js'
+import vue from './node_modules/@vitejs/plugin-vue/dist/index'
 
 // https://vite.dev/config/
 export default defineConfig({
